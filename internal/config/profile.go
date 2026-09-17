74971e6dcc55eabceacd9746c81bce359d6c735d
gerden <public.gerden@gmail.com>
2026-09-17 08:14:51 +0000

    Code format

--- internal/config/profile.go
+++ internal/config/profile.go
@@ -58,7 +58,7 @@ const DefaultFrameDenyResponse = "ws_policy"
 var (
 	nameRe   = regexp.MustCompile(`^[A-Za-z0-9][A-Za-z0-9._-]*$`)
 	methodRe = regexp.MustCompile(`^[A-Z]+$`)
-	codeRe   = regexp.MustCompile(`^[A-Za-z0-9_.:-]+$`)
+	codeRe   = regexp.MustCompile(`^[A-Z][A-Z0-9_]{0,63}$`)
 )
 
 type Duration int
@@ -705,12 +705,8 @@ func checkCode(code string) error {
 		return nil
 	}
 
-	if len(code) > 64 {
-		return fmt.Errorf("code is longer than 64 bytes")
-	}
-
 	if !codeRe.MatchString(code) {
-		return fmt.Errorf("bad code %q", code)
+		return fmt.Errorf("code %q is not [A-Z][A-Z0-9_]{0,63}", code)
 	}
 
 	return nil
